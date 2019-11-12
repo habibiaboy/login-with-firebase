a012ae7111b493e2eb9130a9eb19738b24fe1922
habibiaboy <mrifqihabibi@gmail.com>
2019-11-12 14:10:59 +0700

    update title

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
       <section class="col-12 col-sm-6 col-md-3">
         
           <div class="form-container">
-            <h3>Firebase Login Example</h3>
+            <h3>Firebase Login Example 2</h3>
             <div class="form-group">
               <label for="exampleInputEmail1">Email address</label>
               <input type="email" placeholder="Email..." id="email_field" class="form-control" />
